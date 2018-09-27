02eb30943df1880dc4d606ece723238d6f2b84cf
Yiheng <development.henryquan@gmail.com>
2018-09-27 22:46:59 +1000

    move scripts out and update runscript

--- legit/runtest.sh
+++ legit/runtest.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-echo this script will run all tests inside scripts/
+echo Start
+
+max="09"
+# able to run up to 100 tests, should be enough
+for i in {0..9}; do
+  for j in {0..9}; do
+    if [ $max -eq "$i$j" ]; then
+      echo Completed
+      exit 0
+    else
+      # run tests
+      echo ===$i$j===
+      ./test$i$j.sh
+    fi
+  done
+done
---
+++ legit/test00.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo Testing subset 0
