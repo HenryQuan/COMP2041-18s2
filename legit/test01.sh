74df2373b3bc8a149d2aa83fb5e6bf2538554d2e
Yiheng <development.henryquan@gmail.com>
2018-10-01 13:19:40 +1000

    add test01 for subset 0 last test

--- legit/test01.sh
+++ legit/test01.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+echo === subset 0 test 7 ===
+./clean.sh
+./legit.pl init
+echo 1 >a
+./legit.pl add a
+./legit.pl commit -m message1
+touch a
+./legit.pl add a
+./legit.pl commit -m message2
+echo === END ===
